b75085bf12525cd9457a187d873a742a07436903
Milan D Fernando <151904151+MilanFernando@users.noreply.github.com>
2024-06-27 22:16:16 +0530

    Initial commit

---
+++ src/edu/ijse/mvc/db/DBconnection.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.db;
+
+/**
+ *
+ * @author Milan
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+    private static DBconnection dBconnection;
+    private Connection connection;
+    private DBconnection() throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "1234");
+ }
+    public static DBconnection getinstance() throws SQLException, ClassNotFoundException{
+        if (dBconnection==null){
+            dBconnection=new DBconnection();
+        }
+            return dBconnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+}
+
+
