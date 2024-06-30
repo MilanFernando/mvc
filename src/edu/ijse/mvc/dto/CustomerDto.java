f5c2b6afe8c673bfc81931848f9338931241ebc4
Milan D Fernando <151904151+MilanFernando@users.noreply.github.com>
2024-06-30 15:45:11 +0530

    create customer dto

---
+++ src/edu/ijse/mvc/dto/CustomerDto.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.dto;
+
+/**
+ *
+ * @author Milan
+ */
+public class CustomerDto {
+    private String CustID;
+    private String CustTitle ;
+    private String CustName ;
+    private String DOB ;
+    private Double salary ;
+    private String CustAddress ;
+    private String City ;
+    private String Province ;
+    private String PostalCode;
+
+    /**
+     * @return the CustID
+     */
+    public String getCustID() {
+        return CustID;
+    }
+
+    /**
+     * @param CustID the CustID to set
+     */
+    public void setCustID(String CustID) {
+        this.CustID = CustID;
+    }
+
+    /**
+     * @return the CustTitle
+     */
+    public String getCustTitle() {
+        return CustTitle;
+    }
+
+    /**
+     * @param CustTitle the CustTitle to set
+     */
+    public void setCustTitle(String CustTitle) {
+        this.CustTitle = CustTitle;
+    }
+
+    /**
+     * @return the CustName
+     */
+    public String getCustName() {
+        return CustName;
+    }
+
+    /**
+     * @param CustName the CustName to set
+     */
+    public void setCustName(String CustName) {
+        this.CustName = CustName;
+    }
+
+    /**
+     * @return the DOB
+     */
+    public String getDOB() {
+        return DOB;
+    }
+
+    /**
+     * @param DOB the DOB to set
+     */
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    /**
+     * @return the salary
+     */
+    public Double getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * @return the CustAddress
+     */
+    public String getCustAddress() {
+        return CustAddress;
+    }
+
+    /**
+     * @param CustAddress the CustAddress to set
+     */
+    public void setCustAddress(String CustAddress) {
+        this.CustAddress = CustAddress;
+    }
+
+    /**
+     * @return the City
+     */
+    public String getCity() {
+        return City;
+    }
+
+    /**
+     * @param City the City to set
+     */
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    /**
+     * @return the Province
+     */
+    public String getProvince() {
+        return Province;
+    }
+
+    /**
+     * @param Province the Province to set
+     */
+    public void setProvince(String Province) {
+        this.Province = Province;
+    }
+
+    /**
+     * @return the PostalCode
+     */
+    public String getPostalCode() {
+        return PostalCode;
+    }
+
+    /**
+     * @param PostalCode the PostalCode to set
+     */
+    public void setPostalCode(String PostalCode) {
+        this.PostalCode = PostalCode;
+    }
+    
+
+    
+}
