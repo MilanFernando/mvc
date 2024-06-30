432d14939df68ef9ffc54c94356541a4133d051c
Milan D Fernando <151904151+MilanFernando@users.noreply.github.com>
2024-06-30 16:35:52 +0530

    create save customer method

--- src/edu/ijse/mvc/model/Customermodel.java
+++ src/edu/ijse/mvc/model/Customermodel.java
@@ -4,10 +4,21 @@
  */
 package edu.ijse.mvc.model;
 
+import edu.ijse.mvc.db.DBconnection;
+import edu.ijse.mvc.dto.CustomerDto;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Prepaired;
+
 /**
  *
  * @author Milan
  */
 public class Customermodel {
-    
+    public String saveCustomer(CustomerDto customerDto)throws SQLException, ClassNotFoundException{
+    Connection connection= DBconnection.getinstance().getConnection();
+      String sql="insert into Customer values(?,?,?,?,?,?,?,?,?,?)";
+      
+      PrepairedStatment statment=connection.prepareStatement(sql);
+       }
 }
