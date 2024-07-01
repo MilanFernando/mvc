35ade5e78b285a8100deb9c10484a1571bf111ce
Milan D Fernando <151904151+MilanFernando@users.noreply.github.com>
2024-07-01 22:48:11 +0530

    create dto(item,item detail)

---
+++ src/edu/ijse/mvc/dto/OrderDetailDto.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.dto;
+
+/**
+ *
+ * @author Milan
+ */
+public class OrderDetailDto {
+    private String orderId;
+    private String itemId;
+    private Integer qty;
+    private Integer discount;
+
+    /**
+     * @return the orderId
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * @return the itemId
+     */
+    public String getItemId() {
+        return itemId;
+    }
+
+    /**
+     * @param itemId the itemId to set
+     */
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    /**
+     * @return the qty
+     */
+    public Integer getQty() {
+        return qty;
+    }
+
+    /**
+     * @param qty the qty to set
+     */
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    /**
+     * @return the discount
+     */
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    /**
+     * @param discount the discount to set
+     */
+    public void setDiscount(Integer discount) {
+        this.discount = discount;
+    }
+    
+}
---
+++ src/edu/ijse/mvc/dto/OrderDto.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.mvc.dto;
+
+/**
+ *
+ * @author Milan
+ */
+public class OrderDto {
+    private String orderId;
+    private String orderDate;
+    private String custId;
+
+    /**
+     * @return the orderId
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * @return the orderDate
+     */
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    /**
+     * @param orderDate the orderDate to set
+     */
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    /**
+     * @return the custId
+     */
+    public String getCustId() {
+        return custId;
+    }
+
+    /**
+     * @param custId the custId to set
+     */
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDto{" + "orderId=" + orderId + ", orderDate=" + orderDate + ", custId=" + custId + '}';
+    }
+    
+    
+}
